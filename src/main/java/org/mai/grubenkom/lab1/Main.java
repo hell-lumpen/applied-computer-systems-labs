02bd702b61c0f2e678f4bb03647e1c428d06b768
Grubenko Maxim <mdgrubenko@maieducation>
2022-12-02 23:23:05 +0300

    some changes

--- src/main/java/org/mai/grubenkom/lab1/Main.java
+++ src/main/java/org/mai/grubenkom/lab1/Main.java
@@ -1,7 +1,5 @@
 package org.mai.grubenkom.lab1;
 
-import org.mai.grubenkom.lab1.BracketChecker;
-
 public class Main {
     public static void main(String[] args) {
         BracketChecker checker = new BracketChecker("src/main/resources/brackets.txt");
---
+++ src/main/java/org/mai/grubenkom/lab2/Main.java
@@ -0,0 +1,7 @@
+package org.mai.grubenkom.lab2;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
