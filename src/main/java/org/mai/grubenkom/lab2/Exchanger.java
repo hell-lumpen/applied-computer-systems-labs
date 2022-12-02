87b76d5a39fb23d4ba8e175f2efc6f6870d502b8
Grubenko Maxim <mdgrubenko@maieducation>
2022-12-03 01:07:04 +0300

    add lab #2

---
+++ src/main/java/org/mai/grubenkom/lab2/Exchanger.java
@@ -0,0 +1,86 @@
+package org.mai.grubenkom.lab2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
+@Data
+@AllArgsConstructor
+class Coin {
+    private int denomination;
+    private int quantity;
+}
+
+@Data
+class ExchangeResult {
+    private HashMap<Integer, Integer> exchangeMap;
+    private boolean success;
+    ExchangeResult() {
+        exchangeMap = new HashMap<>();
+        success = false;
+    }
+//    public void put(Integer denomination, Integer countOfCoins) {
+//        exchangeMap.put(denomination, countOfCoins);
+//    }
+//    public void clear() {
+//        exchangeMap.clear();
+//    }
+}
+
+public class Exchanger {
+    private final ArrayList<Coin> coinList;
+    private final ExchangeResult exchangeResult;
+
+    Exchanger() {
+        coinList = new ArrayList<>();
+        exchangeResult = new ExchangeResult();
+
+        coinList.add(new Coin(5, 3));
+        coinList.add(new Coin(10, 1));
+        coinList.add(new Coin(1, 3));
+
+        coinList.sort(Comparator.comparingInt(Coin::getDenomination).reversed());
+    }
+    private ExchangeResult _exchange(int amountOfMoney, int coinListIndex) {
+        int countOfCoins = 0;
+        int denomination = coinList.get(coinListIndex).getDenomination();
+        int quantity = coinList.get(coinListIndex).getQuantity();
+
+        while (amountOfMoney >= denomination && quantity != 0) {
+            countOfCoins++;
+            amountOfMoney -= denomination;
+            quantity--;
+        }
+
+        if (countOfCoins != 0) {
+            exchangeResult.getExchangeMap().put(denomination, countOfCoins);
+        }
+        if (amountOfMoney != 0 && coinList.size() > coinListIndex + 1) {
+            return _exchange(amountOfMoney, coinListIndex + 1);
+        } else if (amountOfMoney != 0 && coinList.size() <= coinListIndex + 1){
+            exchangeResult.getExchangeMap().clear();
+            exchangeResult.setSuccess(false);
+            return exchangeResult;
+        }
+        exchangeResult.setSuccess(true);
+        return exchangeResult;
+    }
+
+    public void exchange(int amountOfMoney) {
+        System.out.printf("Change money in denominations of %d\n", amountOfMoney);
+        System.out.println("Available for exchange:");
+        coinList.forEach((coin) -> System.out.println(coin.getQuantity() + " coins in denominations of " + coin.getDenomination()));
+        ExchangeResult result = _exchange(amountOfMoney, 0);
+        if (exchangeResult.isSuccess()) {
+            System.out.println("Exchanged!");
+            result.getExchangeMap().forEach((key, value) -> System.out.println(value + " coins in denominations of " + key));
+        } else {
+            System.err.println("No exchange possible");
+        }
+    }
+}
--- src/main/java/org/mai/grubenkom/lab2/Main.java
+++ src/main/java/org/mai/grubenkom/lab2/Main.java
@@ -1,7 +1,10 @@
 package org.mai.grubenkom.lab2;
 
+import java.util.HashMap;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    Exchanger exchanger = new Exchanger();
+    exchanger.exchange(29);
   }
 }
