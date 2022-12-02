e4db2623e6f17d2512f42aeb8110e345e08c4e60
Grubenko Maxim <mdgrubenko@maieducation>
2022-12-02 11:57:59 +0300

    init

---
+++ src/main/java/org/mai/grubenkom/Main.java
@@ -0,0 +1,10 @@
+package org.mai.grubenkom;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "hello world";
+        String str1 = "hello world";
+        System.out.println(str.hashCode());
+        System.out.println(str1.hashCode());
+    }
+}
